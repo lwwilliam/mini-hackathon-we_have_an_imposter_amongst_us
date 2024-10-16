524faafac9d517959ec9e7dcf060336832da2f48
SpaghettiCodes <chuashixiang@gmail.com>
2024-10-16 09:09:16 +0800

    kill me

--- frontend/src/view/jobDescriptions/jobDescriptions.jsx
+++ frontend/src/view/jobDescriptions/jobDescriptions.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { QualPriority, JobMode, JobType } from '../../constants';
-import { JobDescriptionModal } from './jobDescriptionModal';
+import { JobDescriptionModal } from './jobDescriptionModal.jsx'
 import UploadPDFModal from '../../components/pdfUpload/uploadPdf';
 import { useNavigate } from 'react-router-dom';
 
