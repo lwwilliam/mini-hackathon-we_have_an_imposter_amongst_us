c54216f2a19853f8a820f0ce06f3152e53aaa3a0
Ivan Laptop <ivan.teohjh@gmail.com>
2024-10-16 09:10:27 +0800

    clean redundant, rename text

--- frontend/src/view/pageHeader/typeHeader.tsx
+++ frontend/src/view/pageHeader/typeHeader.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const PageHeader: React.FC = () => {
     return (
-        <div id="header" className='bg-[#1e1e1e]/[.58] w-screen h-16 flex gap-x-8 text-center font-semibold'>
-            <div className="ml-[50px]"></div>
-            <a href="/" className="cursor-pointer my-4 mx-6 text-2xl text-white hover:underline">Resume</a>
-            <a href="/job" className="cursor-pointer my-4 mx-6 text-2xl text-white hover:underline">Job Descriptions</a>
+        <div id="header" className='bg-[#1e1e1e]/[.58] w-screen h-16 flex gap-x-8 text-center font-semibold pl-[50px]'>
+            <a href="/" className="cursor-pointer my-4 mx-6 text-2xl text-white hover:underline">Resumes</a>
+            <a href="/job" className="cursor-pointer my-4 mx-6 text-2xl text-white hover:underline">Jobs</a>
         </div>
     );
 }
