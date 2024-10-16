c39deb91128ccc022114db14c50009076ca59404
SpaghettiCodes <chuashixiang@gmail.com>
2024-10-16 10:58:02 +0800

    added tags and seperated job desc with resume due to different logics

---
+++ frontend/src/components/JDUploader/JDUploader.jsx
@@ -0,0 +1,124 @@
+import { useEffect, useRef, useState } from "react";
+import PDFUploader from "../pdfUpload/uploadPdf";
+import Modal from "../Modal";
+
+const TagDivs = ({tagname, clicked, onClick}) => {
+	const clickHandler = () => {
+		onClick()
+	}
+
+	return (
+	<div 
+	className={`px-3 border-1 border-black border-solid rounded-xl ${clicked ? 'bg-green-500' : 'bg-white'}`}
+	onClick={clickHandler}
+	>
+		{tagname}
+	</div>
+	)
+}
+
+const JDUploader = ({ open, onClose }) => {
+	const modalRef = useRef(null);
+  const [procesState, setProcessing] = useState(false)
+	const [allTags, setAllTags] = useState([])
+	const [tagArray, setTag] = useState([])
+
+	const getAllTags = async () => {
+		const request = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/getAllTags`)
+		const response = await request.json()
+		if (!request.ok) {
+			console.error("Shit, panic")
+			return 
+		}
+		const newArray = (response).map((val) => val.tag_name)
+		setAllTags(newArray)
+	}
+
+	useEffect(() => {
+		getAllTags()
+	}, [])
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
+
+	const modifyTagArray = (name) => {
+		console.log(tagArray)
+		console.log(name)
+		const newTagArray = tagArray.slice()
+		const index = newTagArray.indexOf(name)
+		console.log(index)
+		if (index < 0)
+			newTagArray.push(name)
+		else
+			newTagArray.splice(index, 1);
+		setTag(newTagArray)
+	}
+
+	const uploadPDF = async (files) => {
+		console.log(tagArray)
+		const file = files[0]
+
+		console.log(file)
+
+		const url = `${process.env.REACT_APP_BACKEND_URL}/api/parseJD`
+		const formData = new FormData()
+		formData.append('File', file)
+
+		try {
+			setProcessing(true)
+			const response = await fetch(url, {
+				method: 'POST',
+				body: formData
+			})
+			const json = await response.json()
+			// uhh heres the message, idk what u wanna do w/ this
+			console.log(json.msg)
+		} catch {
+			alert("Well that failed")
+		}
+
+		setTag([])
+	}
+
+	useEffect(() => {
+		open ? document.addEventListener('mousedown', handleClickOutside) 
+			: document.removeEventListener('mousedown', handleClickOutside)
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [open])
+
+	return (
+		<Modal open={open}>
+			<div ref={modalRef} 
+      className='bg-white rounded-lg h-[35rem] w-[30rem] md:h-[50rem] md:w-[50rem] 
+      flex flex-col justify-center items-center overflow-auto px-72'>
+        { procesState ? <span>Uploading ...</span> : 
+				<PDFUploader 
+					onClose={onClose}
+					setProcessing={setProcessing}
+					onUpload={uploadPDF}
+				>
+					<div className="flex flex-col gap-2">
+						<div>Select Tags: </div>
+						<div className="flex flex-wrap gap-2">
+							{ allTags.map( (val) =>
+								<TagDivs 
+								tagname={val}
+								clicked={tagArray.indexOf(val) >= 0}
+								onClick={() => modifyTagArray(val)}
+							/>) }
+						</div> 
+					</div>
+				</PDFUploader>
+				}
+			</div>
+		</Modal>
+	)
+}
+
+export default JDUploader
--- frontend/src/components/pdfUpload/uploadPdf.jsx
+++ frontend/src/components/pdfUpload/uploadPdf.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Modal from "../Modal.jsx";
 import uploadImage from "../../assets/image.png"
 
 const preventDefaults = (e) => {
@@ -7,48 +6,30 @@ const preventDefaults = (e) => {
   e.stopPropagation()
 }
 
-const PDFUploader = ({onClose, setProcessing, apiURL}) => {
+const PDFUploader = ({onClose, setProcessing, onUpload, children}) => {
   const [ drag, setDrag ] = useState(false)
+  const [ files, setFiles ] = useState([])
   const fileinputRef = useRef(null);
-
+  
   const uploadPDFFile = async () => {
-    const file = fileinputRef.current.files[0] // Temp
-
-    console.log(file)
-
-    const url = apiURL
-    // const url = 'http://localhost:5000/api/ai'
-    const formData = new FormData()
-    formData.append('File', file)
-
-    try {
-      setProcessing(true)
-      const response = await fetch(url, {
-        method: 'POST',
-        body: formData
-      })
-      const json = await response.json()
-      // uhh heres the message, idk what u wanna do w/ this
-      console.log(json.msg)
-    } catch {
-      alert("Well that failed")
-    }
+    setProcessing(true)
+    await onUpload(files)
     setProcessing(false)
     onClose()
-  }
+  } 
 
   const handleDropFile = (e) => {
     e.preventDefault()
 
     const files = e.dataTransfer.files
     fileinputRef.current.files = files
-    uploadPDFFile()
+    setFiles(files)
   }
 
   return (
-  <>
+  <div className='flex flex-col gap-5'>
     <input ref={fileinputRef} type="file" name="upload" accept="application/pdf" id="upload-pdf" className='opacity-0'
-    onChange={uploadPDFFile}
+    onChange={e => setFiles(e.target.files)}
     />
     <label htmlFor="upload-pdf">
       <div 
@@ -78,41 +59,16 @@ const PDFUploader = ({onClose, setProcessing, apiURL}) => {
       >
         <img src={uploadImage}></img>
         <span className='text-center'>
-          Drop files here to upload
+          { files.length ? `${files.length} files added` : 'Drop files here to upload'}
         </span>
       </div>
     </label>
-  </>)
+    {children}
+    <button
+    className='bg-white flex-1 rounded-xl border-solid border-black border-2 py-1'
+    onClick={uploadPDFFile}
+    >Submit</button>
+  </div>)
 }
 
-const UploadPDFModal = ({ open, onClose, apiURL }) => {
-	const modalRef = useRef(null);
-  const [procesState, setProcessing] = useState(false)
-
-  const handleClickOutside = (event) => {
-    if (modalRef.current && !modalRef.current.contains(event.target)) {
-      onClose();
-    }
-  };
-
-	useEffect(() => {
-		open ? document.addEventListener('mousedown', handleClickOutside) 
-			: document.removeEventListener('mousedown', handleClickOutside)
-
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside)
-		}
-	}, [open])
-
-	return (
-		<Modal open={open}>
-			<div ref={modalRef} 
-      className='bg-white rounded-lg h-[35rem] w-[30rem] md:h-[50rem] md:w-[50rem] 
-      flex flex-col justify-center items-center overflow-auto'>
-        { procesState ? <span>Uploading ...</span> : <PDFUploader onClose={onClose} setProcessing={setProcessing} apiURL={apiURL} /> }
-			</div>
-		</Modal>
-	)
-}
-
-export default UploadPDFModal
+export default PDFUploader
